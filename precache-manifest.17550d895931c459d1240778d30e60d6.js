8ac45c835a7152320545075ab0dc0b10f27c2b5e
Mithun <joieeemithun98@gmail.com>
2020-10-19 01:44:50 +0530

    Updates

---
+++ precache-manifest.17550d895931c459d1240778d30e60d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50746d7e72a26a54a04bd9efbb6ca3c8",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "f9d012a55ee7342f4843",
+    "url": "/react-monsters/static/css/main.43dd6c04.chunk.css"
+  },
+  {
+    "revision": "5a5b5d4d61784f131205",
+    "url": "/react-monsters/static/js/2.89420f1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters/static/js/2.89420f1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9d012a55ee7342f4843",
+    "url": "/react-monsters/static/js/main.e67d8fcf.chunk.js"
+  },
+  {
+    "revision": "9d969b64dcdfb07a8d65",
+    "url": "/react-monsters/static/js/runtime-main.285e01b7.js"
+  }
+]);
